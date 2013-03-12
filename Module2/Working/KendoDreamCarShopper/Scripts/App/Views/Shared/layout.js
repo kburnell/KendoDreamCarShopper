778c4285e071d8cbdbc91c728276b6a01ce5d4d2
Keith Burnell <kburnell@skylinetechnologies.com>
2013-03-12 09:39:25 -0500

    Progress: Recording Module 2

---
+++ Module2/Working/KendoDreamCarShopper/Scripts/App/Views/Shared/layout.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    $("#menu").kendoMenu();
+    $.ajax({
+        url: "/Api/Makes/",
+        dataType: "json",
+        type: "GET",
+        success: function (makes) {
+            makes.forEach(function (make) {
+                var markup = "<li><a href='/Makes/Index/" + make.Id + "'>" + make.Name + "</a></li>";
+                $("#makesMenu").append(markup);
+            });
+            $("#menu").kendoMenu();
+        }
+    });
+});
