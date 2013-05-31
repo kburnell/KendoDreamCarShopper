a1f50752d5e3e0f84f247f9d8204c3f3ad1afcb2
Keith Burnell <kburnell@skylinetechnologies.com>
2013-05-31 11:30:23 -0500

    Progress: Module 3 (Home screen)

---
+++ Module3/End/KendoDreamCarShopper/Scripts/App/Views/Home/index.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    var viewModel = new kendo.Observable({
+        modelImages: []
+    });
+    var mainImages = new kendo.data.DataSource({
+        transport: {
+            read: {
+                url: "/Api/MainImages/",
+                dataType: "json"
+            }
+        }
+    });
+    mainImages.fetch(function() {
+        viewModel.mainImages = mainImages.data();
+        kendo.bind("#body", viewModel);
+    });
+    var makeImages = new kendo.data.DataSource({
+         transport: {
+            read: {
+                url: "/Api/Makes/",
+                dataType: "json"
+            }
+        }
+    });
+    $("#makeImages").kendoListView({
+        dataSource: makeImages,
+        template: kendo.template($("#makesTemplate").html())
+    });
+});
